8b6beb316b17de8bf5ee8e9ae06454baae5a2e77
MTB <moritz.brechtel@edu.rptu.de>
2024-02-07 18:55:15 +0100

    Dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use serenity::prelude::*;
+use serenity::model::gateway::Ready;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} ist verbunden!", ready.user.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Laden der Umgebungsvariablen aus der .env-Datei
+    dotenv::dotenv().ok();
+
+    // Extrahieren des Discord-Bot-Tokens aus den Umgebungsvariablen
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Fehler beim Laden des Bot-Tokens aus der Umgebungsvariablen");
+
+    // Erstellt einen neuen Discord-Client mit dem geladenen Token
+    let mut client = Client::new(&token, Handler)
+        .expect("Fehler beim Erstellen des Clients");
+
+    // Startet den Discord-Bot
+    if let Err(why) = client.start() {
+        println!("Fehler beim Starten des Bots: {:?}", why);
+    }
 }
